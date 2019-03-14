82687fb93d61434977345eb81e5178421731c81b
Liem_Dazkun <liem.dazkun@live.com>
2019-03-14 16:17:53 +0100

    añadido mvn y java de entradas

---
+++ src/main/java/lfr/ld/files/joinFiles/JoinFiles.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package lfr.ld.files.joinFiles;
+
+/**
+ * @author lfloresr
+ *
+ */
+public class JoinFiles {
+
+  /**
+   * java -jar JoinFiles -i 
+   * -i carpeta de entrada
+   * -d carpeta de destino
+   * -p password
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
