08f7c43c327e35bb6b98c94272fc169d22f28896
Liem_Dazkun <liem.dazkun@live.com>
2019-03-27 22:14:43 +0100

    amplicion de la clase principal

--- src/main/java/lfr/ld/files/joinFiles/JoinFiles.java
+++ src/main/java/lfr/ld/files/joinFiles/JoinFiles.java
@@ -1,13 +1,18 @@
-/**
- * 
- */
 package lfr.ld.files.joinFiles;
+
+import java.util.HashMap;
+import java.util.List;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import lfr.ld.files.joinFiles.utils.Archivos;
 
 /**
  * @author lfloresr
  *
  */
 public class JoinFiles {
+
+  private static Logger logger = Logger.getLogger(JoinFiles.class);
 
   /**
    * java -jar JoinFiles -i 
@@ -17,7 +22,16 @@ public class JoinFiles {
    * @param args
    */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    BasicConfigurator.configure();
+    logger.info("JoinFiles.main::INI");
+    HashMap<String, List<String>> archivos = new HashMap<String, List<String>>();
+    try {
+        Arguments arg = Arguments.getInstance(args);
+        archivos = Archivos.readFiles();
+    } catch (Exception e) {
+      logger.fatal(e);
+    }
+    logger.info("JoinFiles.main::FIN");
 
   }
 
